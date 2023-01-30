b1900d8a0872bf6b9cd382d99a57bbb42f3c7dfd
Pro100Vlad1047 <91087864+Pro100Vlad1047@users.noreply.github.com>
2023-01-30 15:16:28 +0200

    Add files via upload

--- ToDo/JS/appi.js
+++ ToDo/JS/appi.js
@@ -3,32 +3,42 @@ let addInput=document.querySelector("input");
 let addButton=document.querySelector("button");
 let amounter=0;
 
-addButton.addEventListener("click",function(){
-  if(addInput.value!=""&& amounter!=15){
+addButton.addEventListener("click",addTasks);
+document.addEventListener("keyup",function(e){
+  if(e.code=="Enter"){
+    console.log(e.code);
+    addTasks();
+  }
+});
+
+function delTask(e){
+  e.closest("li").remove();
+  --amounter;
+  
+}
+
+function addTasks(){
+  if(addInput.value!=""&& amounter!=19){
     let matchArr=taskList.textContent.includes(addInput.value);
     //console.log(matchArr);
     if (matchArr==false) {
       taskList.insertAdjacentHTML("beforeend",`<li>${addInput.value}<button onclick="delTask(this)" class="button_delete">Delete task</button></li>`)
       addInput.value="";
       addInput.placeholder="Write your next task!";
+      addInput.focus();
       ++amounter;
     //console.log(taskList.textContent);
     }else{
       addInput.value="";
       addInput.placeholder="This task already exist,sorry!Try another one!";
+      addInput.focus();
     }
-  }else if(amounter==15){
+  }else if(amounter==19){
     addInput.value="";
     addInput.placeholder="Stop it ,It`s a lot of tasks!!!!";
   }else if(addInput.value==""){
     addInput.placeholder="Maybe you try to write smthing,no?";
+    addInput.focus();
    // console.log(taskList.textContent);
   }
-
-})
-
-function delTask(e){
-  e.closest("li").remove();
-  --amounter;
-  
 }
